63eb128bd8977daca1a46bf216b4c2396f358802
Eddie <eocampo52@gmail.com>
2024-10-17 10:57:29 -0500

    added requireLogin middleware function to app

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const User = require("./models/user");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-const session = require('express-session')
+const session = require("express-session");
 
 mongoose.connect("mongodb://127.0.0.1:27017/authDemo");
 
@@ -11,7 +11,14 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(express.urlencoded({ extended: true }));
-app.use(session({secret: 'notagoodsecret'}))
+app.use(session({ secret: "notagoodsecret" }));
+
+const requireLogin = (req, res, next) => {
+  if (!req.session.user_id) {
+    return res.redirect("/login");
+  }
+  next();
+};
 
 app.get("/", (req, res) => {
   res.send("this is the homepage");
@@ -29,7 +36,7 @@ app.post("/register", async (req, res) => {
     password: hash,
   });
   await user.save();
-  req.session.user_id = user._id
+  req.session.user_id = user._id;
   res.redirect("/secret");
 });
 
@@ -42,24 +49,21 @@ app.post("/login", async (req, res) => {
   const user = await User.findOne({ username });
   const validPassword = await bcrypt.compare(password, user.password);
   if (validPassword) {
-    req.session.user_id = user._id
-    res.redirect('/secret');
+    req.session.user_id = user._id;
+    res.redirect("/secret");
   } else {
     res.redirect("/login");
   }
 });
 
-app.post('/logout', (req, res) => {
+app.post("/logout", (req, res) => {
   // req.session.user_id = null;
-  req.session.destroy()
-  res.redirect('/login') 
-})
+  req.session.destroy();
+  res.redirect("/login");
+});
 
-app.get("/secret", (req, res) => {
-  if(!req.session.user_id){
-   return res.redirect('/login')
-  }
-  res.render('secret')
+app.get("/secret", requireLogin, (req, res) => {
+  res.render("secret");
 });
 
 app.listen(3000, () => {
